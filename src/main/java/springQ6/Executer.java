8d3998373e5a26ab2e355dca7b9958fa0788a155
Shubham <shubham.rawat@tothenew.com>
2017-07-04 22:44:53 +0530

    Q6's commit

---
+++ src/main/java/springQ6/Executer.java
@@ -0,0 +1,16 @@
+package springQ6;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+
+public class Executer {
+    public static void main(String[] args){
+
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-config.xml");
+        MyDemoClass6 myDemoClass6 = applicationContext.getBean(MyDemoClass6.class);
+        myDemoClass6.display();
+
+    }
+}
